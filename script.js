6c7e97735c355aa6e6771d868f6327b136bbaf61
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2025-04-12 06:01:44 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth*0.98;
+canvas.height = window.innerHeight*0.98;
+
+var c = canvas.getContext('2d');
+
+c.fillRect(100, 100, 100, 100);
