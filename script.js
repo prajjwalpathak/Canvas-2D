b8a5a9e4b4bffa6e0ea30d35c28718595080019a
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2025-05-08 19:44:59 +0530

    Body CSS correction

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 var canvas = document.querySelector("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth * 0.96;
+canvas.height = window.innerHeight * 0.96;
 
 var c = canvas.getContext("2d");
 
 // Rectangle
-c.fillStyle = "rgb(0, 82, 171)";
-c.fillRect(100, 100, 100, 100);
-c.fillStyle = "#990000";
-c.fillRect(200, 200, 400, 400);
+// c.fillStyle = "rgb(0, 82, 171)";
+// c.fillRect(100, 100, 100, 100);
+// c.fillStyle = "#990000";
+// c.fillRect(200, 200, 400, 400);
 
 // Line
-var x1 = 100;
-var y1 = 100;
-var x2 = 4 * x1;
-var y2 = y1;
-var x3 = x2;
-var y3 = 4 * y2;
-var x4 = x1;
-var y4 = y3;
+// var x1 = 100;
+// var y1 = 100;
+// var x2 = 4 * x1;
+// var y2 = y1;
+// var x3 = x2;
+// var y3 = 4 * y2;
+// var x4 = x1;
+// var y4 = y3;
 
-c.beginPath();
-c.moveTo(x1, y1);
-c.lineTo(x2, y2);
-c.lineTo(x3, y3);
-c.lineTo(x4, y4);
-c.closePath();
-c.strokeStyle = "lime";
-c.stroke();
+// c.beginPath();
+// c.moveTo(x1, y1);
+// c.lineTo(x2, y2);
+// c.lineTo(x3, y3);
+// c.lineTo(x4, y4);
+// c.closePath();
+// c.strokeStyle = "lime";
+// c.stroke();
 
 // Arc
 var PI = 22 / 7;
@@ -39,9 +39,11 @@ const drawCircle = (x, y, radius) => {
 };
 
 // Draw random circles
-for (let i = 0; i < 5; i++) {
-  let randomX = Math.random() * window.innerWidth * 0.8;
-  let randomY = Math.random() * window.innerHeight * 0.8;
-  let randomRadius = Math.random() * 100;
-  drawCircle(randomX, randomY, randomRadius);
-}
+// for (let i = 0; i < 5; i++) {
+//   let randomX = Math.random() * window.innerWidth * 0.8;
+//   let randomY = Math.random() * window.innerHeight * 0.8;
+//   let randomRadius = Math.random() * 100;
+//   drawCircle(randomX, randomY, randomRadius);
+// }
+
+drawCircle(500, 500, 100);
