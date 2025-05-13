fd73bd7df3a7dcb474eba95111d0e0fdc2e7f458
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2025-05-13 21:25:54 +0530

    Added script_gravity, test gravity

---
+++ script_gravity.js
@@ -0,0 +1,6 @@
+let PI = 22 / 7;
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth * 0.96;
+canvas.height = window.innerHeight * 0.96;
+
+let c = canvas.getContext("2d");
