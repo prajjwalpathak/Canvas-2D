bacca6ed26381486e9dfa7de20f1d1fc1829b672
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2025-05-13 21:43:58 +0530

    Add getRandom function

--- script_gravity.js
+++ script_gravity.js
@@ -4,3 +4,7 @@ canvas.width = window.innerWidth * 0.96;
 canvas.height = window.innerHeight * 0.96;
 
 let c = canvas.getContext("2d");
+
+const getRandom = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
